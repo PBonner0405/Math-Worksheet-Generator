afca37593b024080bce179a7ab0ea2468cb71758
topdev10 <k100cool@outlook.com>
2020-02-14 12:40:38 +0800

    build header

--- src/pages/footer/index.js
+++ src/pages/footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            Footer
+            Contact Me: +1 123 456 7890
         </footer>
     )
 }
--- src/pages/header/index.js
+++ src/pages/header/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import styles from './header.module.css';
+
+import Logo from '../../assets/icons/logo.png';
 
 const Header = () => {
+    console.log(styles)
     return (
-        <header>
-            Header
+        <header className={styles.wrapper}>
+            <a href="/" className={styles.favicon}>
+                <img alt="logo" src={Logo} />
+            </a>
+            <ul className={styles.links}>
+                <li>
+                    <a href="/home">
+                        Home Page
+                    </a>
+                </li>
+                <li>
+                    <a href="/main">
+                        Get My Sheet
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                         About US
+                    </a>
+                </li>
+            </ul>
         </header>
     )
 }
